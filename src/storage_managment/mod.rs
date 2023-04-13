9507ee92026e083b885e28da3ffc9ff778f1fffe
vova <frol_off@meta.ua>
2023-04-13 21:07:01 +0300

    start

---
+++ src/storage_managment/mod.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = crate::GlobalContext)]
+pub struct StorageManagement {
+    #[interactive_clap(subcommand)]
+    storage_actions: StorageActions,
+}
+
+#[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = crate::GlobalContext)]
+#[strum_discriminants(derive(EnumMessage, EnumIter))]
+/// What do you want to do with the storage?
+pub enum StorageActions {
+    #[strum_discriminants(strum(
+        message = "view-storage-balance    - View storage balance for an account"
+    ))]
+    /// View storage balance for an account
+    ViewStorageBalance,
+    #[strum_discriminants(strum(
+        message = "storage-deposit         - Make a storage deposit for the account"
+    ))]
+    /// Make a storage deposit for the account
+    StorageDeposit,
+    #[strum_discriminants(strum(
+        message = "create-account          - Withdraw storage for the account"
+    ))]
+    /// Withdraw storage for the account
+    StorageWithdraw,
+}

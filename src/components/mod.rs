84956d6865beac715303b14ed724568bb94503f3
FroVolod <36816899+FroVolod@users.noreply.github.com>
2023-04-20 18:01:02 +0300

    refactor: Renamed to bos-cli (#28)

---
+++ src/components/mod.rs
@@ -0,0 +1,26 @@
+use strum::{EnumDiscriminants, EnumIter, EnumMessage};
+
+mod deploy;
+mod download;
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = crate::GlobalContext)]
+pub struct Components {
+    #[interactive_clap(subcommand)]
+    command: self::ComponentsCommand,
+}
+
+#[derive(Debug, EnumDiscriminants, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = crate::GlobalContext)]
+#[strum_discriminants(derive(EnumMessage, EnumIter))]
+/// What are you up to?
+pub enum ComponentsCommand {
+    #[strum_discriminants(strum(message = "download -   Download widgets from account"))]
+    /// Download widgets from account
+    Download(self::download::AccountId),
+    #[strum_discriminants(strum(
+        message = "deploy               -   Deploy widget if code has changed"
+    ))]
+    /// Deploy widget if code has changed
+    Deploy(self::deploy::DeployToAccount),
+}

e16fb9734fcd86775f919860bcd8f2a808cbc706
FroVolod <frol_off@meta.ua>
2023-05-13 20:50:38 +0300

    renamed DeleleteWidgets

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -16,15 +16,13 @@ pub struct Components {
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 /// What are you up to?
 pub enum ComponentsCommand {
-    #[strum_discriminants(strum(message = "download -   Download widgets from account"))]
+    #[strum_discriminants(strum(message = "download   -   Download widgets from account"))]
     /// Download widgets from account
     Download(self::download::AccountId),
-    #[strum_discriminants(strum(
-        message = "deploy               -   Deploy widget if code has changed"
-    ))]
+    #[strum_discriminants(strum(message = "deploy     -   Deploy widget if code has changed"))]
     /// Deploy widget if code has changed
     Deploy(self::deploy::DeployToAccount),
-    #[strum_discriminants(strum(message = "delete               -   Delete widgets"))]
-    /// Delete widgets
-    DeleteWidgets(self::delete::DeleleteWidgetsFromAccount),
+    #[strum_discriminants(strum(message = "delete     -   Delete widgets from account"))]
+    /// Delete widgets from account
+    Delete(self::delete::DeleleteWidgetsFromAccount),
 }

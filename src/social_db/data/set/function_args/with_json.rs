3ca38b549b2279d2263958424d7855fcbf5c162b
FroVolod <frol_off@meta.ua>
2023-05-26 19:38:14 +0300

    added json type

--- src/social_db/data/set/function_args/with_json.rs
+++ src/social_db/data/set/function_args/with_json.rs
@@ -1,15 +1,9 @@
-use std::str::FromStr;
-
-use color_eyre::eyre::Context;
-use inquire::Text;
-
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(input_context = super::super::SetContext)]
 #[interactive_clap(output_context = FunctionArgsContext)]
 pub struct FunctionArgs {
-    #[interactive_clap(skip_default_input_arg)]
     /// Enter the arguments to this function:
-    args: String,
+    args: near_cli_rs::types::json::Json,
     #[interactive_clap(named_arg)]
     /// Specify signer account ID
     sign_as: super::super::sign_as::Signer,
@@ -23,13 +17,11 @@ impl FunctionArgsContext {
         previous_context: super::super::SetContext,
         scope: &<FunctionArgs as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
     ) -> color_eyre::eyre::Result<Self> {
-        let value =
-            serde_json::Value::from_str(&scope.args).wrap_err("Data not in JSON format!")?;
         Ok(Self(super::ArgsContext {
             config: previous_context.config,
             set_to_account_id: previous_context.set_to_account_id,
             key: previous_context.key,
-            value,
+            value: scope.args.clone().into(),
         }))
     }
 }
@@ -37,18 +29,5 @@ impl FunctionArgsContext {
 impl From<FunctionArgsContext> for super::ArgsContext {
     fn from(item: FunctionArgsContext) -> Self {
         item.0
-    }
-}
-
-impl FunctionArgs {
-    fn input_args(_context: &super::super::SetContext) -> color_eyre::eyre::Result<Option<String>> {
-        loop {
-            let args = Text::new("Enter the arguments to this function:").prompt()?;
-            if serde_json::Value::from_str(&args).is_err() {
-                println!("Data not in JSON format!");
-            } else {
-                return Ok(Some(args));
-            }
-        }
     }
 }

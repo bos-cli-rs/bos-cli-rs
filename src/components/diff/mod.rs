0c7e03b74e950113a1f77b6ec210ceec89a5b8cd
FroVolod <36816899+FroVolod@users.noreply.github.com>
2023-10-22 12:11:06 +0300

    feat: A new command for diff-ing the widgets code (#56)
    Resolved #7

---
+++ src/components/diff/mod.rs
@@ -0,0 +1,89 @@
+use color_eyre::eyre::ContextCompat;
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = near_cli_rs::GlobalContext)]
+#[interactive_clap(output_context = DiffContext)]
+pub struct Diff {
+    #[interactive_clap(skip_default_input_arg)]
+    /// On which account do you want to compare local components?
+    account_id: near_cli_rs::types::account_id::AccountId,
+    #[interactive_clap(named_arg)]
+    /// Select network
+    network_config: near_cli_rs::network::Network,
+}
+
+#[derive(Clone)]
+pub struct DiffContext(near_cli_rs::network::NetworkContext);
+
+impl DiffContext {
+    pub fn from_previous_context(
+        previous_context: near_cli_rs::GlobalContext,
+        scope: &<Diff as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        let account_id: near_primitives::types::AccountId = scope.account_id.clone().into();
+        let on_after_getting_network_callback: near_cli_rs::network::OnAfterGettingNetworkCallback =
+            std::sync::Arc::new({
+                let account_id = account_id.clone();
+                move |network_config| {
+                    let near_social_account_id = crate::consts::NEAR_SOCIAL_ACCOUNT_ID
+                        .get(network_config.network_name.as_str())
+                        .wrap_err_with(|| {
+                            format!(
+                                "The <{}> network does not have a near-social contract.",
+                                network_config.network_name
+                            )
+                        })?;
+
+                    let local_components = crate::common::get_local_components()?;
+                    if local_components.is_empty() {
+                        println!("There are no components in the current ./src folder. Goodbye.");
+                        return Ok(());
+                    }
+                    let local_component_name_list = local_components.keys().collect::<Vec<_>>();
+
+                    let remote_components = crate::common::get_remote_components(
+                        network_config,
+                        local_component_name_list,
+                        near_social_account_id,
+                        &account_id,
+                    )?;
+
+                    if !remote_components.is_empty() {
+                        let updated_components = crate::common::get_updated_components(
+                            local_components,
+                            &remote_components,
+                        );
+                        if updated_components.is_empty() {
+                            println!("There are no new or modified components in the current ./src folder. Goodbye.");
+                            return Ok(());
+                        }
+                    } else {
+                        println!("\nAll local components are new to <{account_id}>.");
+                    };
+                    Ok(())
+                }
+            });
+        Ok(Self(near_cli_rs::network::NetworkContext {
+            config: previous_context.config,
+            interacting_with_account_ids: vec![account_id],
+            on_after_getting_network_callback,
+        }))
+    }
+}
+
+impl From<DiffContext> for near_cli_rs::network::NetworkContext {
+    fn from(item: DiffContext) -> Self {
+        item.0
+    }
+}
+
+impl Diff {
+    pub fn input_account_id(
+        context: &near_cli_rs::GlobalContext,
+    ) -> color_eyre::eyre::Result<Option<near_cli_rs::types::account_id::AccountId>> {
+        near_cli_rs::common::input_non_signer_account_id_from_used_account_list(
+            &context.config.credentials_home_dir,
+            "On which account do you want to compare local components?",
+        )
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,6 +2,7 @@ use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
 mod delete;
 mod deploy;
+mod diff;
 mod download;
 
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
@@ -16,15 +17,20 @@ pub struct Components {
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
 /// What are you up to?
 pub enum ComponentsCommand {
-    #[strum_discriminants(strum(message = "download   -   Download components from account"))]
+    #[strum_discriminants(strum(message = "download    -   Download components from account"))]
     /// Download components from account
     Download(self::download::AccountId),
     #[strum_discriminants(strum(
-        message = "deploy     -   Deploy components if code has changed"
+        message = "diff        -   Differences between component code for deployment"
+    ))]
+    /// Differences between component code for deployment
+    Diff(self::diff::Diff),
+    #[strum_discriminants(strum(
+        message = "deploy      -   Deploy components if code has changed"
     ))]
     /// Deploy сomponents if code has changed
     Deploy(self::deploy::DeployToAccount),
-    #[strum_discriminants(strum(message = "delete     -   Delete components from account"))]
+    #[strum_discriminants(strum(message = "delete      -   Delete components from account"))]
     /// Delete components from account
     Delete(self::delete::DeleteComponentsFromAccount),
 }

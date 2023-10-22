0ee3c5d8ed9bed37e865fd7435405bb97d22472c
FroVolod <frol_off@meta.ua>
2023-10-22 09:56:13 +0300

    updated input_account_id() for "permissions"

--- src/social_db/permissions/grant_write_access/account_id/mod.rs
+++ src/social_db/permissions/grant_write_access/account_id/mod.rs
@@ -1,7 +1,11 @@
+use color_eyre::eyre::ContextCompat;
+use inquire::Select;
+
 #[derive(Debug, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(input_context = super::SocialDbKeyContext)]
 #[interactive_clap(output_context = AccessToAccountContext)]
 pub struct AccessToAccount {
+    #[interactive_clap(skip_default_input_arg)]
     /// Enter the account ID you will grant permission to:
     account_id: near_cli_rs::types::account_id::AccountId,
     #[interactive_clap(named_arg)]
@@ -28,5 +32,46 @@ impl AccessToAccountContext {
 impl From<AccessToAccountContext> for super::storage_deposit::AccessToPermissionKeyContext {
     fn from(item: AccessToAccountContext) -> Self {
         item.0
+    }
+}
+
+impl AccessToAccount {
+    pub fn input_account_id(
+        context: &super::SocialDbKeyContext,
+    ) -> color_eyre::eyre::Result<Option<near_cli_rs::types::account_id::AccountId>> {
+        loop {
+            let deploy_to_account_id =
+                near_cli_rs::common::input_non_signer_account_id_from_used_account_list(
+                    &context.global_context.config.credentials_home_dir,
+                    "Enter the account ID you will grant permission to:",
+                )?
+                .wrap_err("Internal error!")?;
+            if !near_cli_rs::common::is_account_exist(
+                &context.global_context.config.network_connection,
+                deploy_to_account_id.clone().into(),
+            ) {
+                println!(
+                    "\nThe account <{}> does not yet exist.",
+                    &deploy_to_account_id
+                );
+                #[derive(strum_macros::Display)]
+                enum ConfirmOptions {
+                    #[strum(to_string = "Yes, I want to enter a new account name.")]
+                    Yes,
+                    #[strum(to_string = "No, I want to use this account name.")]
+                    No,
+                }
+                let select_choose_input = Select::new(
+                    "Do you want to enter a new account name?",
+                    vec![ConfirmOptions::Yes, ConfirmOptions::No],
+                )
+                .prompt()?;
+                if let ConfirmOptions::No = select_choose_input {
+                    return Ok(Some(deploy_to_account_id));
+                }
+            } else {
+                return Ok(Some(deploy_to_account_id));
+            }
+        }
     }
 }
